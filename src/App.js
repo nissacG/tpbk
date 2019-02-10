261e92b67e2f60a12c098fc5f5372c5b5eabee52
Gareth Cassin <nissacjg@gmail.com>
2019-02-10 21:46:44 +0000

    initial commit - coming soon

--- src/App.js
+++ src/App.js
@@ -1,28 +1,20 @@
-import React, { Component } from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, { Component } from 'react'
+import './styles/App.css'
+
+import logo from './assets/logo.png'
 
 class App extends Component {
   render() {
     return (
       <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <p>
-            Edit <code>src/App.js</code> and save to reload.
-          </p>
-          <a
-            className="App-link"
-            href="https://reactjs.org"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Learn React
-          </a>
+        <header>
+          The Plant Based Kitchen
         </header>
+        <img src={logo} alt='the plant based kitchen logo' style={{ height: '100px' }} />
+        <p>Coming Soon...</p>
       </div>
-    );
+    )
   }
 }
 
-export default App;
+export default App
