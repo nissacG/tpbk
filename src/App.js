8c616b0a762a14e1ee9eef863d273095b1ca1b08
Gareth Cassin <nissacjg@gmail.com>
2019-02-10 22:17:05 +0000

    remove logo, add new font

--- src/App.js
+++ src/App.js
@@ -1,8 +1,6 @@
 import React, { Component } from 'react'
 import './styles/App.css'
 
-import logo from './assets/logo.png'
-
 class App extends Component {
   render() {
     return (
@@ -10,7 +8,6 @@ class App extends Component {
         <header>
           The Plant Based Kitchen
         </header>
-        <img src={logo} alt='the plant based kitchen logo' style={{ height: '100px' }} />
         <p>Coming Soon...</p>
       </div>
     )
