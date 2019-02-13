d12035d3ecd43a69f2b4e2d3928ec15d8472343f
Gareth Cassin <nissacjg@gmail.com>
2019-02-13 18:25:20 +0000

    initial header and footer components

--- src/App.js
+++ src/App.js
@@ -1,14 +1,18 @@
 import React, { Component } from 'react'
 import './styles/App.css'
+import Header from './components/Header'
+import Footer from './components/Footer'
 
 class App extends Component {
   render() {
     return (
       <div className="App">
+        <Header />
         <header>
           The Plant Based Kitchen
         </header>
         <p>Coming Soon...</p>
+        <Footer />
       </div>
     )
   }
