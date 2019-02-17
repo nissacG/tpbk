64e6241924c76c90573f84985691cfe346051854
Gareth Cassin <nissacjg@gmail.com>
2019-02-17 21:32:52 +0000

    trying different fonts - still WIP

--- src/App.js
+++ src/App.js
@@ -9,6 +9,9 @@ class App extends Component {
           The Plant Based Kitchen
         </header>
         <p>Coming Soon...</p>
+        {/* <p>
+          The alarm still oscillated, louder here, the rear wall dulling the roar of the car’s floor. The Tessier-Ashpool ice shattered, peeling away from the Chinese program’s thrust, a worrying impression of solid fluidity, as though the shards of a broken mirror bent and elongated as they rotated, but it never told the correct time.
+        </p> */}
       </div>
     )
   }
