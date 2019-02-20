59f29eb4c165c889e2fff6a394b67b330f76a2a0
Gareth Cassin <nissacjg@gmail.com>
2019-02-20 19:00:57 +0000

    fix footer position

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -3,7 +3,7 @@ import React, { Component } from 'react'
 class Footer extends Component {
   render() {
     return (
-      <div>
+      <div className='footer' style={{position: 'fixed', bottom: '0', left: '0', width: '100%'}}>
         Footer comp
       </div>
     )
